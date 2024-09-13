d10f438825a492f133930672d2e96f319da94340
areebatahi <areebatahirarain@gmail.com>
2024-09-14 01:04:36 +0500

    messege

--- app.js
+++ app.js
@@ -8,6 +8,33 @@ function registration() {
     var cpassword = document.getElementById('cpassword');
     // console.log(name.value, email.value, phoneNumber.value, password.value, cpassword.value)
 
+    var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+    if (password.value !== cpassword.value) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "password should be same!",
+            footer: '<a href="#">Why do I have this issue?</a>'
+        });
+    } else if (!regex.test(password.value)) {
+        Swal.fire(`At least one lowercase alphabet i.e. [a-z]
+        At least one uppercase alphabet i.e. [A-Z]
+        At least one Numeric digit i.e. [0-9]
+        At least one special character i.e. [‘@’, ‘$’, ‘.’, ‘#’, ‘!’, ‘%’, ‘*’, ‘?’, ‘&’, ‘^’]
+        Also, the total length must be in the range [8-15]`);
+    } else {
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Registration Successfully",
+            showConfirmButton: false,
+            timer: 1500
+        });
+        setTimeout(() => {
+            window.location.href = "./dashboard.html"
+        }, 2000);
+    }
+
     var userData = {
         name: name.value,
         email: email.value,
@@ -21,9 +48,7 @@ function registration() {
     // var getData = localStorage.getItem("userData");
     // var parseData = JSON.parse(getData);
     // console.log(parseData);
-    setTimeout(() => {
-        window.location.href = "./dashboard.html"
-    }, 2);
+
 }
 
 function getLocalData() {
@@ -45,17 +70,36 @@ function redirect() {
     window.location.href = "./index.html"
 }
 
-function login(){
+function login() {
+    event.preventDefault()
     var email = document.getElementById('email');
     var password = document.getElementById('password');
     var getData = localStorage.getItem("userData");
     var parseData = JSON.parse(getData);
-    console.log(parseData);
 
-    if(parseData.email !== email){
-        alert('Invalid Email');
-    }else if(parseData.password !== parseData){
-        alert('Invalid Email');
+    if (parseData.email !== email.value) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Invalid Email",
+            footer: '<a href="#">Why do I have this issue?</a>'
+        });
+    } else if (parseData.password !== password.value) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Invalid passward",
+            footer: '<a href="#">Why do I have this issue?</a>'
+        });
+    } else {
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Login Succussfully",
+            showConfirmButton: false,
+            timer: 1500
+        });
+        window.location.href = "./dashboard.html"
     }
 
 }
