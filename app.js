57b9b057ba136c2ef048268b591b470a023a6190
areebatahi <areebatahirarain@gmail.com>
2024-09-13 17:26:57 +0500

    class_practice_23

---
+++ app.js
@@ -0,0 +1,46 @@
+
+function registration() {
+    event.preventDefault()
+    var name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var phoneNumber = document.getElementById('phoneNumber');
+    var password = document.getElementById('password');
+    var cpassword = document.getElementById('cpassword');
+    // console.log(name.value, email.value, phoneNumber.value, password.value, cpassword.value)
+
+    var userData = {
+        name: name.value,
+        email: email.value,
+        phoneNumber: phoneNumber.value,
+        password: password.value,
+        cpassword: cpassword.value
+    }
+    // console.log(userData);
+
+    localStorage.setItem("userData", JSON.stringify(userData));
+    // var getData = localStorage.getItem("userData");
+    // var parseData = JSON.parse(getData);
+    // console.log(parseData);
+    setTimeout(() => {
+        window.location.href = "./dashboard.html"
+    }, 2);
+}
+
+function getLocalData() {
+    var getLocalDataDiv = document.getElementById('getLocalDataDiv');
+    var getData = localStorage.getItem("userData");
+    var parseData = JSON.parse(getData);
+    console.log(parseData)
+    getLocalDataDiv.innerHTML = `
+    <ul>
+      <li>Name: ${parseData.name} </li>
+      <li>Email: ${parseData.email} </li>
+      <li>Phone Number: ${parseData.phoneNumber} </li>
+    </ul>
+    `
+}
+getLocalData()
+
+function logOut() {
+    window.location.href = "./index.html"
+}
